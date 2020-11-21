7cfeed7d74b3cfba05c94f7ff6d11306c5884d39
Drew Carlson <awcarlson@protonmail.com>
2020-11-21 14:14:42 -0800

    Use shared global json instance

--- src/desktopCommonTest/kotlin/desktopTestImpl.kt
+++
@@ -1,6 +0,0 @@
-package drewcarlson.coingecko
-
-import io.ktor.client.engine.HttpClientEngineFactory
-import io.ktor.client.engine.curl.Curl
-
-actual fun createHttpEngine(): HttpClientEngineFactory<*> = Curl
