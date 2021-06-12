7b43ab8c0f674a0672e498bc5da29d8e94616b8f
Drew Carlson <awcarlson@protonmail.com>
2021-06-12 14:11:32 -0700

    Locale independent CoinPrice apis, fix nullability

--- src/commonMain/kotlin/drewcarlson/coingecko/models/coins/CoinPrice.kt
+++ src/commonMain/kotlin/drewcarlson/coingecko/models/coins/CoinPrice.kt
@@ -10,25 +10,25 @@ data class CoinPrice(
         checkNotNull(getPriceOrNull(currency))
 
     fun getPriceOrNull(currency: String): Double? =
-        rawFields[currency.toLowerCase()]?.toDoubleOrNull()
+        rawFields[currency.lowercase()]?.toDoubleOrNull()
 
     fun getMarketCap(currency: String): Double =
         checkNotNull(getMarketCapOrNull(currency))
 
     fun getMarketCapOrNull(currency: String): Double? =
-        rawFields["${currency.toLowerCase()}_market_cap"]?.toDoubleOrNull()
+        rawFields["${currency.lowercase()}_market_cap"]?.toDoubleOrNull()
 
-    fun get24hrVol(currency: String): Double? =
+    fun get24hrVol(currency: String): Double =
         checkNotNull(get24hrVolOrNull(currency))
 
     fun get24hrVolOrNull(currency: String): Double? =
-        rawFields["${currency.toLowerCase()}_24h_vol"]?.toDoubleOrNull()
+        rawFields["${currency.lowercase()}_24h_vol"]?.toDoubleOrNull()
 
-    fun get24hrChange(currency: String): Double? =
+    fun get24hrChange(currency: String): Double =
         checkNotNull(get24hrChangeOrNull(currency))
 
     fun get24hrChangeOrNull(currency: String): Double? =
-        rawFields["${currency.toLowerCase()}_24h_change"]?.toDoubleOrNull()
+        rawFields["${currency.lowercase()}_24h_change"]?.toDoubleOrNull()
 
     fun getRawField(key: String): String? = rawFields[key]
 
