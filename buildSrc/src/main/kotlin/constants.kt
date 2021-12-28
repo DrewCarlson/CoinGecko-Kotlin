c5eb5695eb2bddbccb086b78e3747bdfcd4748e7
Drew Carlson <awcarlson@protonmail.com>
2021-12-27 21:05:33 -0800

    Use coroutines native-mt

--- buildSrc/src/main/kotlin/constants.kt
+++ buildSrc/src/main/kotlin/constants.kt
@@ -1,4 +1,4 @@
 const val KOTLIN_VERSION = "1.6.10"
 const val KTOR_VERSION = "1.6.7"
-const val COROUTINES_VERSION = "1.6.0"
+const val COROUTINES_VERSION = "1.6.0-native-mt"
 const val SERIALIZATION_VERSION = "1.3.2"
