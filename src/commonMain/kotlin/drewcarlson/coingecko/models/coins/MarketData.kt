d50d573b00e55b6cd0ae88cad263d823e749af6b
Drew Carlson <awcarlson@protonmail.com>
2022-07-15 08:52:44 -0700

    Use empty maps instead of null for MarketData fields

--- src/commonMain/kotlin/drewcarlson/coingecko/models/coins/MarketData.kt
+++ src/commonMain/kotlin/drewcarlson/coingecko/models/coins/MarketData.kt
@@ -8,24 +8,24 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class MarketData(
         @SerialName("current_price")
-        val currentPrice: Map<String, Double>? = null,
+        val currentPrice: Map<String, Double> = emptyMap(),
         val roi: Roi? = null,
-        val ath: Map<String, Double>? = null,
+        val ath: Map<String, Double> = emptyMap(),
         @SerialName("ath_change_percentage")
-        val athChangePercentage: Map<String, Double>? = null,
+        val athChangePercentage: Map<String, Double> = emptyMap(),
         @SerialName("ath_date")
-        val athDate: Map<String, String>? = null,
-        val atl: Map<String, Double>? = null,
+        val athDate: Map<String, String> = emptyMap(),
+        val atl: Map<String, Double> = emptyMap(),
         @SerialName("atl_change_percentage")
-        val atlChangePercentage: Map<String, Double>? = null,
+        val atlChangePercentage: Map<String, Double> = emptyMap(),
         @SerialName("atl_date")
-        val atlDate: Map<String, String>? = null,
+        val atlDate: Map<String, String> = emptyMap(),
         @SerialName("market_cap")
-        val marketCap: Map<String, Double>? = null,
+        val marketCap: Map<String, Double> = emptyMap(),
         @SerialName("market_cap_rank")
         val marketCapRank: Long = 0,
         @SerialName("total_volume")
-        val totalVolume: Map<String, Double>? = null,
+        val totalVolume: Map<String, Double> = emptyMap(),
         @Serializable(NullValueOmittingMapSerializer::class)
         @SerialName("high_24h")
         val high24h: Map<String, Double> = emptyMap(),
@@ -53,27 +53,27 @@ data class MarketData(
         @SerialName("market_cap_change_percentage_24h")
         val marketCapChangePercentage24h: Double = 0.0,
         @SerialName("price_change_24h_in_currency")
-        val priceChange24hInCurrency: Map<String, Double>? = null,
+        val priceChange24hInCurrency: Map<String, Double> = emptyMap(),
         @SerialName("price_change_percentage_1h_in_currency")
-        val priceChangePercentage1hInCurrency: Map<String, Double>? = null,
+        val priceChangePercentage1hInCurrency: Map<String, Double> = emptyMap(),
         @SerialName("price_change_percentage_24h_in_currency")
-        val priceChangePercentage24hInCurrency: Map<String, Double>? = null,
+        val priceChangePercentage24hInCurrency: Map<String, Double> = emptyMap(),
         @SerialName("price_change_percentage_7d_in_currency")
-        val priceChangePercentage7dInCurrency: Map<String, Double>? = null,
+        val priceChangePercentage7dInCurrency: Map<String, Double> = emptyMap(),
         @SerialName("price_change_percentage_14d_in_currency")
-        val priceChangePercentage14dInCurrency: Map<String, Double>? = null,
+        val priceChangePercentage14dInCurrency: Map<String, Double> = emptyMap(),
         @SerialName("price_change_percentage_30d_in_currency")
-        val priceChangePercentage30dInCurrency: Map<String, Double>? = null,
+        val priceChangePercentage30dInCurrency: Map<String, Double> = emptyMap(),
         @SerialName("price_change_percentage_60d_in_currency")
-        val priceChangePercentage60dInCurrency: Map<String, Double>? = null,
+        val priceChangePercentage60dInCurrency: Map<String, Double> = emptyMap(),
         @SerialName("price_change_percentage_200d_in_currency")
-        val priceChangePercentage200dInCurrency: Map<String, Double>? = null,
+        val priceChangePercentage200dInCurrency: Map<String, Double> = emptyMap(),
         @SerialName("price_change_percentage_1y_in_currency")
-        val priceChangePercentage1yInCurrency: Map<String, Double>? = null,
+        val priceChangePercentage1yInCurrency: Map<String, Double> = emptyMap(),
         @SerialName("market_cap_change_24h_in_currency")
-        val marketCapChange24hInCurrency: Map<String, Double>? = null,
+        val marketCapChange24hInCurrency: Map<String, Double> = emptyMap(),
         @SerialName("market_cap_change_percentage_24h_in_currency")
-        val marketCapChangePercentage24hInCurrency: Map<String, Double>? = null,
+        val marketCapChangePercentage24hInCurrency: Map<String, Double> = emptyMap(),
         @SerialName("total_supply")
         val totalSupply: Double? = null,
         @SerialName("circulating_supply")
